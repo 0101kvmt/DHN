c35f0ae5094cd39c319c86e165f6f7ba18b5fb89
0101kvmt <0101kvmt@gmail.com>
2018-01-05 19:05:40 -0800

    shotgun worksgit add .!

--- Weps/ShotGun.cs
+++ Weps/ShotGun.cs
@@ -7,23 +7,25 @@ public class ShotGun : MonoBehaviour
 {
 
     public GameObject BloodSplat;
-    public Sprite idlePistol;
-    public Sprite shotPistol;
+    public Sprite idleShotGun;
+    public Sprite shotShotGun;
     public float shotGunDamage;
     public float shotGunRange;
     public AudioClip shotSound;
     public AudioClip reloadSound;
     public AudioClip emptyGunSound;
 
+    public float shotGunCount = 10;
     public float fireRate = 0.5F;
-    public float nextFire = 0.0F;
+    public float nextFire = 0.15F;
+    public float spreadFactor = 15;
     public Text ammoText;
 
     public Animator anim;
 
     public int ammoAmount;
     public int ammoClipSize;
-
+    
 
     public GameObject bulletHole;
 
@@ -60,44 +62,63 @@ public class ShotGun : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Vector2 bulletOffset = Random.insideUnitCircle * 10;
-        //  Vector2 bulletOffset = Random.insideUnitCircle * DynamicCrossHair.spread;
-        Vector3 randomTarget = new Vector3(Screen.width / 2 + bulletOffset.x, Screen.height / 2 + bulletOffset.y, 0);
-        randomTarget.x += Random.Range(-spreadFactor, spreadFactor);
-        Vector3 randomTarget = new Vector3()
-        Ray ray = Camera.main.ScreenPointToRay(randomTarget);
-        RaycastHit hit;
-        if (isShot == true && ammoClipLeft > 0 && isReloading == false)
-        {
-            isShot = false;
-            DynamicCrossHair.spread += DynamicCrossHair.SHOTGUN_SHOOTING_SPREAD;
-            ammoClipLeft--;
-            source.PlayOneShot(shotSound);
-            StartCoroutine("shot");
-            if (Physics.Raycast(ray, out hit, shotGunRange))
+      
+           
+            RaycastHit hit;
+            if (isShot == true && ammoClipLeft > 0 && isReloading == false)
             {
-                Debug.Log("Collided at " + hit.collider.gameObject.name);
-                if (hit.transform.CompareTag("Enemy"))
+                
+                for (int s = 0; s < shotGunCount; s++)
                 {
-                    Instantiate(BloodSplat, hit.point, Quaternion.identity);
-                    if (hit.collider.gameObject.GetComponent<EnemyStates>().currentState == hit.collider.gameObject.GetComponent<EnemyStates>().patrolState || hit.collider.gameObject.GetComponent<EnemyStates>().currentState == hit.collider.gameObject.GetComponent<EnemyStates>().alertState)
+                    // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    Vector2 bulletOffset = Random.insideUnitCircle * 10;
+                    //  Vector2 bulletOffset = Random.insideUnitCircle * DynamicCrossHair.spread;
+
+                    Vector3 randomTarget = new Vector3(Screen.width / 2 + bulletOffset.x, Screen.height / 2 + bulletOffset.y, 0);
+
+                    randomTarget.x += Random.Range(-spreadFactor, spreadFactor);
+                    randomTarget.y += Random.Range(-spreadFactor, spreadFactor);
+                    randomTarget.z += Random.Range(-spreadFactor, spreadFactor);
+
+
+                    Ray ray = Camera.main.ScreenPointToRay(randomTarget);
+
+                    DynamicCrossHair.spread += DynamicCrossHair.SHOTGUN_SHOOTING_SPREAD;
+                    ammoClipLeft--;
+                    source.PlayOneShot(shotSound);
+                    StartCoroutine("shot");
+                if (Physics.Raycast(ray, out hit, shotGunRange))
+                {
+                    Debug.Log("Collided at " + hit.collider.gameObject.name);
+                    if (hit.transform.CompareTag("Enemy"))
                     {
-                        hit.collider.gameObject.SendMessage("HiddenShot", transform.parent.transform.position, SendMessageOptions.DontRequireReceiver);
+
+                        Instantiate(bulletHole, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal)).transform.parent = hit.collider.gameObject.transform;
+
+                        Instantiate(BloodSplat, hit.point, Quaternion.identity);
+                        if (hit.collider.gameObject.GetComponent<EnemyStates>().currentState == hit.collider.gameObject.GetComponent<EnemyStates>().patrolState || hit.collider.gameObject.GetComponent<EnemyStates>().currentState == hit.collider.gameObject.GetComponent<EnemyStates>().alertState)
+                        {
+                            hit.collider.gameObject.SendMessage("HiddenShot", transform.parent.transform.position, SendMessageOptions.DontRequireReceiver);
+                        }
+
+                        hit.collider.gameObject.SendMessage("addDamage", shotGunDamage, SendMessageOptions.DontRequireReceiver);
                     }
 
-                    hit.collider.gameObject.SendMessage("addDamage", shotGunDamage, SendMessageOptions.DontRequireReceiver);
+
                 }
-
-
             }
-        }
-        else if (isShot == true && ammoClipLeft <= 0 && isReloading == false)
-        {
-            isShot = false;
-            Reload();
-        }
+
+                    isShot = false;
+               
+               
+            }
+            else if (isShot == true && ammoClipLeft <= 0 && isReloading == false)
+            {
+                isShot = false;
+                Reload();
+            }
     }
+        
 
     void Reload()
     {
